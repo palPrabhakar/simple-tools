3a472f4ed16f9220a78fb5bc093e2a6883c85180
Prabhakar Pal <prabhakar.pal00@gmail.com>
2025-02-20 22:33:01 +0530

    added basic blocks

--- use-and-defs/main.py
+++ use-and-defs/main.py
@@ -8,12 +8,46 @@ import tkinter as tk
 from tkinter import ttk
 
 
+block_terminators_riscv = {'jal', 'jalr', 'beq', 'bne', 'blt',
+                           'bge', 'bltu', 'bgeu', 'ret', 'call',
+                           'j', 'jr', 'ecall', 'ebreak', 'bgt',
+                           'ble', 'bgtu', 'bleu', 'beqz', 'bnez',
+                           'bltz', 'bgez', 'bgtz', 'blez'}
+
+
+def get_bb(instrs):
+    bblocks = []
+    cblock = []
+
+    for ins in instrs:
+        if ins.instr in block_terminators_riscv:
+            ins.set_block(cblock)
+            cblock.append(ins)
+            bblocks.append(cblock)
+            cblock = []
+        elif not ins.dest:
+            if len(cblock) == 0:
+                ins.set_block(cblock)
+                cblock.append(ins)
+            else:
+                bblocks.append(cblock)
+                cblock = [ins]
+        else:
+            ins.set_block(cblock)
+            cblock.append(ins)
+
+    return bblocks
+
+
 class Instruction:
     def __init__(self, instr, operands):
         self.instr = instr
         self.dest = operands[0]
         self.src0 = operands[1]
         self.src1 = operands[2]
+
+    def set_block(self, block):
+        self.block = block
 
     def __str__(self):
         if not self.dest:
@@ -30,15 +64,17 @@ class Function:
     def __init__(self, name, instrs):
         self.name = name
         self.instrs = instrs
+        self.bb = get_bb(self.instrs)
 
     def __str__(self):
         return f"{self.name}"
 
 
 def parse_instr(instr):
-    name, operands = instr.split()
-    operands = [x.strip()
-                for x in operands.split(',')]
+    name, *operands = instr.split()
+    if len(operands) > 0:
+        operands = [x.strip()
+                    for x in operands[0].split(',')]
     return Instruction(name, [*operands, None, None, None])
 
 
@@ -79,7 +115,7 @@ def show(function):
 
 def main():
     print("Def and Use")
-    function = read_assembly("risc-v.asm")
+    function = read_assembly("risc-v2.asm")
     show(function)
 
 
