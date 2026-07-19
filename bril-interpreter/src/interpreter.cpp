b9a6bfc1727e90af9bc8f590382b6f51a43ca4bc
Prabhakar Pal <prabhakar.pal00@gmail.com>
2026-07-19 10:31:11 -0400

    added support for more instruction in bril-interpreter

--- bril-interpreter/src/interpreter.cpp
+++ bril-interpreter/src/interpreter.cpp
@@ -14,6 +14,7 @@ using Value = std::variant<bool, float, int>;
 
 struct Frame {
     std::unordered_map<std::string, Value> variables;
+    std::unordered_map<std::string, size_t> labels;
     std::string name;
     size_t ip;
     size_t end;
@@ -62,6 +63,10 @@ const std::unordered_map<std::string, Interpreter> opcode_list = {
 #define value_int(x) static_cast<int>(x.Get("value")->Get<double>().value())
 #define get_args(x, i) x.Get("args")->Get(i)->Get<std::string>().value()
 #define args_size(x) x.Get("args")->Size()
+#define has_opcode(x) x.Get("op").has_value()
+#define has_label(x) x.Get("label").has_value()
+#define labels_size(x) x.Get("labels")->Size()
+#define get_labels(x, i) x.Get("labels")->Get(i)->Get<std::string>().value()
 
 // frame helpers
 #define get_arg_v(f, in, i) f.variables[get_args(in, i)]
@@ -71,13 +76,15 @@ const std::unordered_map<std::string, Interpreter> opcode_list = {
 #define _frame __frame(ctx)
 #define _inc_ip ++frame.ip
 
+constexpr auto &_iota = std::views::iota;
+
 void interpret_function(Context &ctx, std::string fn_name);
 
 Context create_context(const sjp::Json &program) {
     Context ctx;
 
     auto functions = program.Get("functions");
-    for (auto i : std::views::iota(0u, functions->Size())) {
+    for (auto i : _iota(0u, functions->Size())) {
         auto f = functions->Get(i);
         auto name = f->Get("name")->Get<std::string>();
         ctx.functions.emplace(name.value(), f.value());
@@ -101,7 +108,7 @@ void setup_frame(Context &ctx, std::string fn_name, std::vector<Value> v_args) {
             throw std::runtime_error("args size mismatch\n");
         }
 
-        for (auto i : std::views::iota(0u, args->Size())) {
+        for (auto i : _iota(0u, args->Size())) {
             auto arg = args->Get(i).value();
             auto name = arg.Get("name")->Get<std::string>().value();
             switch (get_type(arg)[0]) {
@@ -120,6 +127,27 @@ void setup_frame(Context &ctx, std::string fn_name, std::vector<Value> v_args) {
     ctx.frames.push(frame);
 }
 
+void find_label(Context &ctx, std::string lbl_name) {
+    _frame;
+
+    const auto func = ctx.functions.at(frame.name);
+    const auto instrs = func.Get("instrs").value();
+
+    size_t i = frame.ip + 1;
+    while (i < frame.end) {
+        auto instr = instrs.Get(i).value();
+        if (has_label(instr)) {
+            auto lbl = instr.Get("label")->Get<std::string>().value();
+            if (lbl == lbl_name) {
+                frame.labels[lbl] = i;
+                return;
+            }
+        }
+    }
+
+    throw std::runtime_error(std::format("label: {} not found\n", lbl_name));
+}
+
 void interpret_program(sjp::Json &program) {
     auto ctx = create_context(program);
     // TODO:
@@ -134,15 +162,21 @@ void interpret_function(Context &ctx, std::string fn_name) {
             std::format("{} function not found\n", fn_name));
     }
 
-    const auto &func = ctx.functions.at(fn_name);
+    const auto func = ctx.functions.at(fn_name);
     const auto instrs = func.Get("instrs").value();
 
     auto &frame = ctx.frames.top();
 
     while (frame.ip < frame.end) {
         const auto instr = instrs.Get(frame.ip).value();
-        const auto opcode = instr.Get("op")->Get<std::string>().value();
-        opcode_list.at(opcode)(ctx, instr);
+
+        if (has_opcode(instr)) {
+            const auto opcode = instr.Get("op")->Get<std::string>().value();
+            opcode_list.at(opcode)(ctx, instr);
+        } else {
+            assert(has_label(instr) && "invalid instruction\n");
+            opcode_list.at("label")(ctx, instr);
+        }
     }
 
     ctx.frames.pop();
@@ -150,7 +184,9 @@ void interpret_function(Context &ctx, std::string fn_name) {
 
 Value get_value(const sjp::Json &instr) {
     switch (get_type(instr)[0]) {
-#define X(t, v) case v: return value_##t(instr);
+#define X(t, v)                                                                \
+    case v:                                                                    \
+        return value_##t(instr);
         TYPE_LIST
 #undef X
     default:
@@ -197,6 +233,36 @@ void interpret_div(Context &ctx, const sjp::Json &instr) {
     _inc_ip;
 }
 
+void interpret_eq(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    set_dest(frame, instr, (operation<std::equal_to, int>(ctx, instr)));
+    _inc_ip;
+}
+
+void interpret_lt(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    set_dest(frame, instr, (operation<std::less, int>(ctx, instr)));
+    _inc_ip;
+}
+
+void interpret_gt(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    set_dest(frame, instr, (operation<std::greater, int>(ctx, instr)));
+    _inc_ip;
+}
+
+void interpret_le(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    set_dest(frame, instr, (operation<std::less_equal, int>(ctx, instr)));
+    _inc_ip;
+}
+
+void interpret_ge(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    set_dest(frame, instr, (operation<std::greater_equal, int>(ctx, instr)));
+    _inc_ip;
+}
+
 void interpret_fadd(Context &ctx, const sjp::Json &instr) {
     _frame;
     set_dest(frame, instr, (operation<std::plus, float>(ctx, instr)));
@@ -221,10 +287,40 @@ void interpret_fdiv(Context &ctx, const sjp::Json &instr) {
     _inc_ip;
 }
 
+void interpret_feq(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    set_dest(frame, instr, (operation<std::equal_to, float>(ctx, instr)));
+    _inc_ip;
+}
+
+void interpret_flt(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    set_dest(frame, instr, (operation<std::less, float>(ctx, instr)));
+    _inc_ip;
+}
+
+void interpret_fgt(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    set_dest(frame, instr, (operation<std::greater, float>(ctx, instr)));
+    _inc_ip;
+}
+
+void interpret_fle(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    set_dest(frame, instr, (operation<std::less_equal, float>(ctx, instr)));
+    _inc_ip;
+}
+
+void interpret_fge(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    set_dest(frame, instr, (operation<std::greater_equal, float>(ctx, instr)));
+    _inc_ip;
+}
+
 void interpret_print(Context &ctx, const sjp::Json &instr) {
     _frame;
     auto size = args_size(instr);
-    for (auto i : std::views::iota(0u, size - 1)) {
+    for (auto i : _iota(0u, size - 1)) {
         std::visit([](auto &&arg) { std::cout << arg; },
                    frame.variables[get_args(instr, i)]);
         std::cout << " ";
@@ -242,4 +338,23 @@ void interpret_ret(Context &ctx, const sjp::Json &instr) {
         ctx.ret_val = get_arg_v(frame, instr, 0);
     }
     frame.ip = frame.end;
+}
+
+void interpret_jmp(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    assert(labels_size(instr) == 1 && "more than one labels in jmp instr\n");
+    auto lbl = get_labels(instr, 0);
+
+    if (!frame.labels.contains(lbl)) {
+        find_label(ctx, lbl);
+    }
+
+    frame.ip = frame.labels[lbl] + 1;
+}
+
+void interpret_label(Context &ctx, const sjp::Json &instr) {
+    _frame;
+    auto lbl_name = instr.Get("label")->Get<std::string>().value();
+    frame.labels[lbl_name] = frame.ip;
+    _inc_ip;
 }
--- bril-interpreter/src/opcode.hpp
+++ bril-interpreter/src/opcode.hpp
@@ -11,15 +11,15 @@
     X(mul, 1)                                                                  \
     X(sub, 1)                                                                  \
     X(div, 1)                                                                  \
-    X(eq, 0)                                                                   \
-    X(lt, 0)                                                                   \
-    X(gt, 0)                                                                   \
-    X(le, 0)                                                                   \
-    X(ge, 0)                                                                   \
+    X(eq, 1)                                                                   \
+    X(lt, 1)                                                                   \
+    X(gt, 1)                                                                   \
+    X(le, 1)                                                                   \
+    X(ge, 1)                                                                   \
     X(and, 0)                                                                  \
     X(or, 0)                                                                   \
     X(not, 0)                                                                  \
-    X(jmp, 0)                                                                  \
+    X(jmp, 1)                                                                  \
     X(br, 0)                                                                   \
     X(call, 0)                                                                 \
     X(ret, 1)                                                                  \
@@ -27,13 +27,13 @@
     X(fmul, 1)                                                                 \
     X(fsub, 1)                                                                 \
     X(fdiv, 1)                                                                 \
-    X(feq, 0)                                                                  \
-    X(flt, 0)                                                                  \
-    X(fgt, 0)                                                                  \
-    X(fle, 0)                                                                  \
-    X(fge, 0)                                                                  \
+    X(feq, 1)                                                                  \
+    X(flt, 1)                                                                  \
+    X(fgt, 1)                                                                  \
+    X(fle, 1)                                                                  \
+    X(fge, 1)                                                                  \
     X(id, 0)                                                                   \
     X(const, 1)                                                                \
     X(print, 1)                                                                \
-    X(label, 0)                                                                \
+    X(label, 1)                                                                \
     X(nop, 0)
